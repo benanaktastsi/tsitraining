d2f3464acc0c4f6816929203ea418e1dfa5164a7
dsalvadortekion <dsalvador@tekion.com>
2024-04-26 09:44:37 +0100

    Gave Order an @Id

--- src/main/java/com/tsi/training/entity/Order.java
+++ src/main/java/com/tsi/training/entity/Order.java
@@ -4,15 +4,17 @@ import com.tsi.training.data.PartId;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.util.List;
 
 @Entity
 @Data
 public class Order {
 
+    @Id
     private String reference;
-    private String orderDate;
-    private List<PartId> partIds;
-    private Long customerId;
-    private Long dealerId;
+        private String orderDate;
+        private List<PartId> partIds;
+        private Long customerId;
+        private Long dealerId;
 }
