5394cf92d2660bee5710b82dfadf093c19925e5a
benanaktastsi <baktas@softwareinstitute.com>
2024-04-26 09:09:53 +0100

    add  dependency

--- src/main/java/com/tsi/training/entity/Customer.java
+++ src/main/java/com/tsi/training/entity/Customer.java
@@ -1,8 +1,13 @@
 package com.tsi.training.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+@Setter
+@Getter
 @Entity
 public class Customer {
 
@@ -10,11 +15,4 @@ public class Customer {
     @Id
     private Long id;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
 }
