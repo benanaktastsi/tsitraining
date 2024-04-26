fe1dbed3319a16ab9b0405128c05cff4396d03ed
benanaktastsi <baktas@softwareinstitute.com>
2024-04-26 15:00:38 +0100

    merge with jsonreader

--- src/main/java/com/tsi/training/JSONataMapper.java
+++ src/main/java/com/tsi/training/JSONataMapper.java
@@ -1,18 +1,16 @@
 package com.tsi.training;
 
-import com.dashjoin.jsonata.Utils;
 import com.google.gson.Gson;
-
-import java.util.LinkedHashMap;
+import com.tsi.training.dto.response.OrderDTO;
 
 import static com.dashjoin.jsonata.Jsonata.jsonata;
 
 public class JSONataMapper {
-    public static Utils.JList Map(String json, String expression) {
+    public static OrderDTO map(String json, String expression) {
         Gson gson = new Gson();
         var data = gson.fromJson(json, Object.class);
 
         var expr = jsonata(expression);
-        return (Utils.JList) expr.evaluate(data);
+        return (OrderDTO) expr.evaluate(data);
     }
 }
