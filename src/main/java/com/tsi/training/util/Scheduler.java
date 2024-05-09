1063f4c8f99159c6c042d74d084bd66a3b9a881f
gboumphrey <74275873+gboumphrey@users.noreply.github.com>
2024-05-09 15:24:01 +0100

    logging updates (#15)
    * changed util.logging to slf4j logging for consistency
    
    * replace sout with log
    
    * Create log4j.properties

--- src/main/java/com/tsi/training/util/Scheduler.java
+++ src/main/java/com/tsi/training/util/Scheduler.java
@@ -1,14 +1,16 @@
 package com.tsi.training.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class Scheduler {
 
     @Scheduled(fixedRate = 10 * 1000)
     public static void ScheduleTest() {
-        System.out.println("Hello World");
+        log.info("Scheduler moment");
     }
 
 }
