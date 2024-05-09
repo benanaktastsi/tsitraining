eb0d97d475c51a905e18c71622fb1e6fa6c8fbd3
TsiComma <dstarkie@softwareinstitute.com>
2024-05-09 10:50:30 +0100

    Added @Component annotation to process order consumer

--- src/main/java/com/tsi/training/util/ProcessOrderConsumer.java
+++ src/main/java/com/tsi/training/util/ProcessOrderConsumer.java
@@ -3,10 +3,12 @@ package com.tsi.training.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.nio.file.Path;
 
+@Component
 public class ProcessOrderConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(ProcessOrderConsumer.class);
@@ -16,6 +18,7 @@ public class ProcessOrderConsumer {
         Path filePath = Path.of("./src/main/resources/" + message);
         try {
             ProcessResponse processed = JSONataMapper.processJSON(filePath);
+            //todo see leo
         } catch (IOException exception){
             log.error(exception.getMessage());
         }
