de53db50b78f52bf83dff33aa9df2d03c974a09e
NadRetsel <danlester.salvador@gmail.com>
2024-05-09 13:06:04 +0100

    Added Kafka producer to send text file name + Experimenting with multiple topics (#12)
    Co-authored-by: dsalvadortekion <dsalvador@tekion.com>

--- src/main/java/com/tsi/training/service/OrderService.java
+++ src/main/java/com/tsi/training/service/OrderService.java
@@ -3,6 +3,7 @@ package com.tsi.training.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +12,19 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class OrderService {
 
+    @Value("${spring.kafka.template.topics}")
+    private final String[] topics;
     public final KafkaTemplate<String, String> kafkaTemplate;
 
 
     public void sendKafkaTopic()
     {
-        log.info("Sending Message topic from Order");
-        kafkaTemplate.send("Message", "ProcessOrders");
+
+        log.info("Sending message from topic [{}]", this.topics[0]);
+        kafkaTemplate.send(this.topics[0], "ProcessOrders");
+
+        log.info("Sending message from topic [{}]", this.topics[1]);
+        kafkaTemplate.send(this.topics[1], "input");
+
     }
 }
