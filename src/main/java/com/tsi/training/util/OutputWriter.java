14b82b792721450169728189d1f75d86aef0573d
Luke-TSI <Luke.Reading@inchcape.com>
2024-05-10 09:32:49 +0100

    Expanded file writer functionality

--- src/main/java/com/tsi/training/util/OutputWriter.java
+++ src/main/java/com/tsi/training/util/OutputWriter.java
@@ -3,13 +3,21 @@ package com.tsi.training.util;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
 public class OutputWriter {
 
-    static void writeToFile(String output, String fileName) throws IOException {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/"+fileName));
-            writer.write(output);
-
-            writer.close();
+    static void writeToFile(Map<String, String> DealerMap) throws IOException {
+            DealerMap.forEach((dealer, JSONString) -> {
+                        String filename = "src/main/resources/"+ dealer;
+                        BufferedWriter writer = null;
+                        try {
+                            writer = new BufferedWriter(new FileWriter(filename));
+                            writer.write(JSONString);
+                            writer.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
     }
 }
