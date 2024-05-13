3197361d05ac9c775bacff13dcd45e7bc1325666
TsiComma <168099579+TsiComma@users.noreply.github.com>
2024-05-13 13:40:11 +0100

    Initialdata (#22)
    * added initial sql commands

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO part (description, price) VALUES ('Tyre',400.00);
+INSERT INTO part (description, price) VALUES ('Clutch', 255.99);
+INSERT INTO part (description, price) VALUES ('Steering', 300.00);
