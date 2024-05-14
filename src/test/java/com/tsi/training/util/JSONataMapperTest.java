b3a7bcea73d90a62d01d9da749a25ff859711f6b
TsiComma <dstarkie@softwareinstitute.com>
2024-05-14 13:53:55 +0100

    added back tests

--- src/test/java/com/tsi/training/util/JSONataMapperTest.java
+++ src/test/java/com/tsi/training/util/JSONataMapperTest.java
@@ -1,23 +1,43 @@
 package com.tsi.training.util;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.nio.file.Path;
 
 @SpringBootTest
 public class JSONataMapperTest {
 
     @Test
-    public void mapFile(){
+    public void readValidFileTest() {
+        Path goodPath = Path.of("./src/main/resources/input/input.json");
         try {
-            ProcessResponse response = JSONataMapper.processJSON(Path.of("./src/main/resources/input.json"));
-            System.out.println("==========================\n PARTS");
-            System.out.println(response.parts);
-            System.out.println("==========================\n ORDERS");
-            System.out.println(response.orders);
+            JSONataMapper.processJSON(goodPath);
+        } catch (Exception e){
+            Assertions.fail("No error should ocuur when reading a correct file");
         }
-        catch (Exception ignored){}
+    }
+
+    @Test
+    public void readMissingFileTest() {
+        Path missingPath = Path.of("./src/main/resources/input/missingfile.json");
+        try {
+            JSONataMapper.processJSON(missingPath);
+        } catch (IOException e) {
+            Assertions.assertInstanceOf(IOException.class, e, "Expected error should be IOException, but was" + e.getClass());
+        }
+    }
+
+    @Test
+    public void readBadFileTest() {
+        Path badPath = Path.of("./src/main/resources/input/baddata.json");
+        try {
+            JSONataMapper.processJSON(badPath);
+        } catch (Exception e){
+            Assertions.assertInstanceOf(ClassCastException.class, e, "Expected error should be ClassCastException, but was" + e.getClass());
+        }
     }
 
 }
