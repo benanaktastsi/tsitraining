393497ff40b382ddbf83891d5f67c1e5fbb9a3a8
dsalvadortekion <dsalvador@tekion.com>
2024-05-15 14:14:12 +0100

    Reformatted PartsCRUDTestCase step-defs

--- src/test/java/com/tsi/training/cucumber/stepdefs/PartsCRUDTestCase.java
+++ src/test/java/com/tsi/training/cucumber/stepdefs/PartsCRUDTestCase.java
@@ -34,7 +34,9 @@ public class PartsCRUDTestCase {
     }
 
 
-
+    // ==================================================
+    // Convert Cucumber inputs to a wrapper object
+    // ==================================================
     @DataTableType
     public PartDTOInputOutputTest convert(Map<String, String> entry)
     {
@@ -73,19 +75,26 @@ public class PartsCRUDTestCase {
     }
 
 
+    // ==================================================
+    // GIVEN steps
+    // ==================================================
+
+    /**
+     * Used during Background
+     */
     @Given("an empty Part Repository")
-    public void aFreshPartRepository() throws Exception
+    public void givenEmptyPartRepository() throws Exception
     {
         this.mockControllerTest.sendNukeRequest();
     }
 
-
-
-    // ==================================================
-    // CREATE
-    // ==================================================
-
-
+    /**
+     * Used by Scenarios:
+     * - CREATE a Part
+     * - READ from Part Repository
+     * - UPDATE a Part
+     * - DELETE a Part
+     */
     @Given("an existing Part Repository with")
     public void givenAnExistingPartRepository(PartDTOInputOutputTest partDTOInputOutputTest) throws Exception
     {
@@ -94,13 +103,39 @@ public class PartsCRUDTestCase {
         for(PartDTO partDTO : partDTOList) this.mockControllerTest.sendPostRequestSavePart(partDTO);
     }
 
-
+    /**
+     * Used by Scenarios:
+     * - CREATE a Part
+     * - READ from Part Repository
+     * - UPDATE a Part
+     * - DELETE a Part
+     */
     @Given("a Part DTO with")
     public void givenPartDTO(PartDTOInputOutputTest partDTOInputOutputTest)
     {
         this.inputPartDTOList = partDTOInputOutputTest.getInputPartDTOList();
     }
 
+    /**
+     * Used by Scenarios:
+     * - UPDATE a Part
+     */
+    @Given("an updated Part DTO with")
+    public void givenUpdatedPartDTO(PartDTOInputOutputTest partDTOInputOutputTest)
+    {
+        this.updatedPartDTOList = new LinkedList<>();
+        this.updatedPartDTOList.addAll(partDTOInputOutputTest.getInputPartDTOList());
+    }
+
+
+    // ==================================================
+    // WHEN steps
+    // ==================================================
+
+    /**
+     * Used by Scenarios:
+     * - CREATE a Part
+     */
     @When("save Part to Part Repository")
     public void whenSavePartToPartRepository() throws Exception
     {
@@ -110,6 +145,74 @@ public class PartsCRUDTestCase {
         }
     }
 
+    /**
+     * Used by Scenarios:
+     * - READ from Part Repository
+     */
+    @When("find Part from Part Repository by description")
+    public void whenFindPartFromPartRepositoryByDescription() throws Exception
+    {
+        this.resultPartDTOList = new LinkedList<>();
+        for(PartDTO inputPartDTO : this.inputPartDTOList) {
+            PartDTO returnedDTO = this.mockControllerTest.sendGetRequestFindPartByDescription(inputPartDTO);
+            if(null != returnedDTO) this.resultPartDTOList.add(returnedDTO);
+        }
+    }
+
+    /**
+     * Used by Scenarios:
+     * - UPDATE a Part
+     */
+    @When("update Part from updated Part DTO")
+    public void whenUpdatePartFromUpdatedPartDTO() throws Exception
+    {
+        for(int i = 0; i < this.inputPartDTOList.size(); i++)
+        {
+            // Get the ID stored in the database (since they're auto-generated)
+            PartDTO originalDTO = this.mockControllerTest.sendGetRequestFindPartByDescription(this.inputPartDTOList.get(i));
+            PartDTO updatedDTO = this.updatedPartDTOList.get(i);
+
+            if(null != originalDTO) this.inputPartDTOList.set(i, originalDTO);
+            updatedDTO.setId(this.inputPartDTOList.get(i).getId());
+
+
+            // Send PATCH request to update
+            this.mockControllerTest.sendPatchRequestUpdatePart(
+                    this.inputPartDTOList.get(i),
+                    updatedDTO);
+        }
+    }
+
+    /**
+     * Used by Scenarios:
+     * - DELETE a Part
+     */
+    @When("delete Part from Part Repository")
+    public void whenDeletePartFromPartRepository() throws Exception {
+        for (int i = 0; i < this.inputPartDTOList.size(); i++) {
+            // Get the ID stored in the database (since they're auto-generated)
+            PartDTO originalDTO = this.mockControllerTest.sendGetRequestFindPartByDescription(this.inputPartDTOList.get(i));
+
+            if (null != originalDTO) this.inputPartDTOList.set(i, originalDTO);
+
+
+            // Send DELETE request to delete
+            this.mockControllerTest.sendDeleteRequestDeletePart(this.inputPartDTOList.get(i));
+
+        }
+    }
+
+
+    // ==================================================
+    // THEN steps
+    // ==================================================
+
+    /**
+     * Used by Scenarios:
+     * - CREATE a Part
+     * - UPDATE a Part
+     * - DELETE a Part
+     */
     @Then("expect Part Repository with")
     public void thenExpectPartRepository(PartDTOInputOutputTest expectedPartDTOInputOutputTest) throws Exception
     {
@@ -132,23 +235,10 @@ public class PartsCRUDTestCase {
 
     }
 
-
-    // ==================================================
-    // READ
-    // ==================================================
-
-
-
-    @When("find Part from Part Repository by description")
-    public void whenFindPartFromPartRepositoryByDescription() throws Exception
-    {
-        this.resultPartDTOList = new LinkedList<>();
-        for(PartDTO inputPartDTO : this.inputPartDTOList) {
-            PartDTO returnedDTO = this.mockControllerTest.sendGetRequestFindPartByDescription(inputPartDTO);
-            if(null != returnedDTO) this.resultPartDTOList.add(returnedDTO);
-        }
-    }
-
+    /**
+     * Used by Scenarios:
+     * - READ from Part Repository
+     */
     @Then("expect Part returned with")
     public void thenExpectPartReturned(PartDTOInputOutputTest expectedPartDTOInputOutputTest)
     {
@@ -161,62 +251,5 @@ public class PartsCRUDTestCase {
         this.resultPartDTOList.forEach(partDTO -> partDTO.setId(null));
 
         Assert.assertEquals(expectedPartDTOList, this.resultPartDTOList);
-    }
-
-
-
-    // ==================================================
-    // UPDATE
-    // ==================================================
-
-
-
-    @Given("an updated Part DTO with")
-    public void givenUpdatedPartDTO(PartDTOInputOutputTest partDTOInputOutputTest)
-    {
-        this.updatedPartDTOList = new LinkedList<>();
-        this.updatedPartDTOList.addAll(partDTOInputOutputTest.getInputPartDTOList());
-    }
-
-    @When("update Part from updated Part DTO")
-    public void whenUpdatePartFromUpdatedPartDTO() throws Exception
-    {
-        for(int i = 0; i < this.inputPartDTOList.size(); i++)
-        {
-            // Get the ID stored in the database (since they're auto-generated)
-            PartDTO originalDTO = this.mockControllerTest.sendGetRequestFindPartByDescription(this.inputPartDTOList.get(i));
-            PartDTO updatedDTO = this.updatedPartDTOList.get(i);
-
-            if(null != originalDTO) this.inputPartDTOList.set(i, originalDTO);
-            updatedDTO.setId(this.inputPartDTOList.get(i).getId());
-
-
-            // Send PATCH request to update
-            this.mockControllerTest.sendPatchRequestUpdatePart(
-                    this.inputPartDTOList.get(i),
-                    updatedDTO);
-        }
-    }
-
-
-    // ==================================================
-    // DELETE
-    // ==================================================
-
-
-    @When("delete Part from Part Repository")
-    public void whenDeletePartFromPartRepository() throws Exception {
-        for (int i = 0; i < this.inputPartDTOList.size(); i++)
-        {
-            // Get the ID stored in the database (since they're auto-generated)
-            PartDTO originalDTO = this.mockControllerTest.sendGetRequestFindPartByDescription(this.inputPartDTOList.get(i));
-
-            if(null != originalDTO) this.inputPartDTOList.set(i, originalDTO);
-
-
-            // Send DELETE request to delete
-            this.mockControllerTest.sendDeleteRequestDeletePart(this.inputPartDTOList.get(i));
-
-        }
     }
 }
