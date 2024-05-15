8ef687362973bf30f2035286591c990069eceeaa
TsiComma <168099579+TsiComma@users.noreply.github.com>
2024-05-15 10:24:54 +0100

    fixed jsonata tests (#32)
    * updated jsonatamapper tests

--- src/test/java/com/tsi/training/util/JSONataMapperTest.java
+++ src/test/java/com/tsi/training/util/JSONataMapperTest.java
@@ -1,42 +1,44 @@
 package com.tsi.training.util;
 
+import com.tsi.training.exception.MissingFileException;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.nio.file.NoSuchFileException;
+import java.io.IOException;
 import java.nio.file.Path;
-
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 public class JSONataMapperTest {
 
     @Test
     public void readValidFileTest() {
-        Path goodPath = Path.of("./src/main/resources/input/input.json");
+        Path goodPath = Path.of("./src/test/resources/input/input.json");
         try {
             JSONataMapper.processJSON(goodPath);
         } catch (Exception e){
-            fail("No error should ocuur when reading a correct file");
+            Assertions.fail("No error should ocuur when reading a correct file");
         }
     }
 
     @Test
     public void readMissingFileTest() {
-        Path missingPath = Path.of("./src/main/resources/input/missingfile.json");
-        assertThrows(NoSuchFileException.class, () -> JSONataMapper.processJSON(missingPath));
+        Path missingPath = Path.of("./src/test/resources/input/missingfile.json");
+        Assertions.assertThrows(
+                MissingFileException.class,
+                () -> JSONataMapper.processJSON(missingPath),
+                "Expected a MissingFileException"
+        );
     }
 
     @Test
     public void readBadFileTest() {
-        Path badPath = Path.of("./src/main/resources/input/baddata.json");
-        try {
-            JSONataMapper.processJSON(badPath);
-        } catch (Exception e){
-            Assertions.assertInstanceOf(ClassCastException.class, e, "Expected error should be ClassCastException, but was" + e.getClass());
-        }
+        Path badPath = Path.of("./src/test/resources/input/baddata.json");
+        Assertions.assertThrows(
+                ClassCastException.class,
+                () -> JSONataMapper.processJSON(badPath),
+                "Expected a ClassCastException"
+        );
     }
 
 }
